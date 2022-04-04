52417cfcaebbf04de896410f1a9e2050e0b08e47
harish <harishbodhu@gmail.com>
2022-04-04 11:53:31 +0530

    comited to delete

---
+++ index.js
@@ -0,0 +1,16 @@
+const card1 = document.getElementsByClassName("card1");
+const card2 = document.getElementsByClassName("card2");
+const card3 = document.getElementsByClassName("card3");
+const btn = document.querySelector("#btn");
+
+const changecolor = (card1) => {
+  //   console.log(card1);
+  let newCard = card1.innnerHTML;
+  newCard = "<p>hi</p>";
+  //   card2.style.display.color == none;
+  //   card3.style.display == none;
+
+  // this file has been changed for thew first
+};
+// changed
+btn.addEventListener("click", changecolor);
